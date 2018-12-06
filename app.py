2c5176b82fd7c8e8281581b04805324d2255958f
ravix339 <ravix339@gmail.com>
2018-12-06 14:36:14 -0500

    added rating, commenting, viewing of public posts and posting

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #Import Flask Library
 from flask import Flask, render_template, request, session, url_for, redirect
+from datetime import datetime, timedelta
 import pymysql.cursors
 
 #Initialize the app from Flask
@@ -7,16 +8,25 @@ app = Flask(__name__)
 
 #Configure MySQL
 conn = pymysql.connect(host='localhost',
-                       port = 8889,
+                       port = 3306,
                        user='root',
-                       password='root',
+                       password='',
                        db='PriCoSha',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=30)
+
 #Define a route to hello function
 @app.route('/')
-def hello():
+def index():
+    if 'email' in session and session['email'][:6] != 'guest_':
+        return redirect(url_for('home'))
+    else:
+        session['email'] = 'guest_' + str(abs(hash(datetime.now())))
     return render_template('index.html')
 
 #Define route for login
@@ -28,6 +38,32 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/home')
+def home():
+    email = session['email']
+    if email[:6]=='guest_':
+        isGuest = 'yes'
+    else:
+        isGuest='no'
+    cursor = conn.cursor()
+    query = 'SELECT post_time, item_id, email_post, file_path, item_name FROM ContentItem WHERE is_pub = %s or is_pub is NULL AND post_time >= DATE_SUB(NOW(), INTERVAL 1 DAY) ORDER BY post_time DESC'
+    cursor.execute(query, ('0'))
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('home.html', email=email, posts=data, isGuest=isGuest)
+
+@app.route('/logout')
+def logout():
+    session.pop('email')
+    return redirect('/')
+        
+app.secret_key = 'some key that you will never guess'
+#Run the app on localhost port 5000
+#debug = True -> you don't have to restart flask
+#for changes to go through, TURN OFF FOR PRODUCTION
+if __name__ == "__main__":
+    app.run('127.0.0.1', 5000, debug = True)
 
 #Authenticates the login
 @app.route('/loginAuth', methods=['GET', 'POST'])
@@ -56,66 +92,107 @@ def loginAuth():
         error = 'Invalid login or email'
         return render_template('login.html', error=error)
 
-@app.route('/home')
-def home():
-    cursor = conn.cursor();
-    query = 'SELECT post_time, item_id, email_post, file_path, item_name FROM ContentItem WHERE is_pub = %s or is_pub is NULL AND post_time >= DATE_SUB(NOW(), INTERVAL 1 DAY) ORDER BY post_time DESC'
-    cursor.execute(query, (0))
-    data = cursor.fetchall()
+#Authenticates the register
+@app.route('/registerAuth', methods=['GET', 'POST'])
+def registerAuth():
+    #grabs information from the forms
+    email = request.form['username']
+    password = request.form['password']
+    fname = request.form['fname']
+    lname = request.form['lname']
+    #cursor used to send queries
+    cursor = conn.cursor()
+    #executes query
+    query = 'SELECT * FROM person WHERE email = %s'
+    cursor.execute(query, (email))
+    #stores the results in a variable
+    data = cursor.fetchone()
+    #use fetchall() if you are expecting more than 1 data row
+    error = None
+    if(data):
+        #If the previous query returns data, then user exists
+        error = "This user already exists"
+        return render_template('register.html', error = error)
+    else:
+        ins = 'INSERT INTO person VALUES(%s, SHA2(%s, 256), %s, %s)'
+        cursor.execute(ins, (email, password, fname, lname))
+        conn.commit()
+        cursor.close()
+        return render_template('index.html')
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    email = session['email']
+    cursor = conn.cursor()
+    item_name = request.form['title']
+    filepath = request.form['filepath']
+    is_pub = request.form['is_pub']
+    if is_pub == '0':
+        query = 'INSERT INTO contentitem (email_post, post_time, file_path, item_name, is_pub) VALUES(%s, CURRENT_TIMESTAMP, %s, %s, NULL)'
+    else:
+        query = 'INSERT INTO contentitem (email_post, post_time, file_path, item_name, is_pub) VALUES(%s, CURRENT_TIMESTAMP, %s, %s, 0)'
+    cursor.execute(query, (email, filepath, item_name))
+    conn.commit()
     cursor.close()
-    return render_template('home.html', email=session['email'], posts=data)
+    return redirect(url_for('home'))
 
-@app.route('/logout')
-def logout():
-    session.pop('email')
-    return redirect('/')
-        
-app.secret_key = 'some key that you will never guess'
-#Run the app on localhost port 5000
-#debug = True -> you don't have to restart flask
-#for changes to go through, TURN OFF FOR PRODUCTION
-if __name__ == "__main__":
-    app.run('127.0.0.1', 5000, debug = True)
+@app.route('/viewContent', methods=['POST'])
+def viewContent():
+    email_add = session['email']
+    item_id = request.form['id']
+    cursor = conn.cursor()
+    query = 'SELECT * FROM contentitem WHERE item_id=%s'
+    cursor.execute(query, (item_id))
+    content_info = cursor.fetchone()
 
+    query = 'SELECT person.email, person.fname, person.lname FROM person WHERE person.email IN (SELECT email_tagged FROM tag WHERE tag.item_id=%s AND tag.status=%s)'
+    cursor.execute(query, (item_id, 'yes'))
+    tag_info = cursor.fetchall()
 
-# #Authenticates the register
-# @app.route('/registerAuth', methods=['GET', 'POST'])
-# def registerAuth():
-#     #grabs information from the forms
-#     email = request.form['email']
-#     password = request.form['password']
+    query = 'SELECT email, rate_time, emoji FROM rate WHERE item_id=%s'
+    cursor.execute(query, (item_id))
+    rating_info = cursor.fetchall()
 
-#     #cursor used to send queries
-#     cursor = conn.cursor()
-#     #executes query
-#     query = 'SELECT * FROM user WHERE email = %s'
-#     cursor.execute(query, (email))
-#     #stores the results in a variable
-#     data = cursor.fetchone()
-#     #use fetchall() if you are expecting more than 1 data row
-#     error = None
-#     if(data):
-#         #If the previous query returns data, then user exists
-#         error = "This user already exists"
-#         return render_template('register.html', error = error)
-#     else:
-#         ins = 'INSERT INTO user VALUES(%s, %s)'
-#         cursor.execute(ins, (email, password))
-#         conn.commit()
-#         cursor.close()
-#         return render_template('index.html')
+    query = 'SELECT email, comment_time, comment FROM comment WHERE item_id=%s'
+    cursor.execute(query, (item_id))
+    comment_info = cursor.fetchall()
 
+    cursor.close()
 
-# @app.route('/post', methods=['GET', 'POST'])
-# def post():
-#     email = session['email']
-#     cursor = conn.cursor();
-#     blog = request.form['blog']
-#     query = 'INSERT INTO blog (blog_post, email) VALUES(%s, %s)'
-#     cursor.execute(query, (blog, email))
-#     conn.commit()
-#     cursor.close()
-#     return redirect(url_for('home'))
+    return render_template('viewContent.html', email=email_add, content=content_info, tag=tag_info, rating=rating_info, comments=comment_info)
+
+@app.route('/rate_comment', methods=['GET', 'POST'])
+def rate_comment():
+    email = session['email']
+    item_id = request.form['id']
+    try:
+        comment = request.form['comment']
+        rate = request.form['rating']
+        cursor = conn.cursor()
+        if comment != '':
+            query = 'INSERT INTO comment (email, comment, item_id) VALUES (%s, %s, %s)'
+            cursor.execute(query, (email, comment, item_id))
+            conn.commit()
+        if rate != '':
+            query = 'SELECT * FROM rate WHERE email=%s AND item_id=%s'
+            cursor.execute(query, (email, item_id))
+            result = cursor.fetchall()
+            if len(result) == 0:
+                query = 'INSERT INTO rate (email, item_id, rate_time, emoji) VALUES (%s, %s, NOW(), %s)'
+                cursor.execute(query, (email, item_id, rate)) 
+                conn.commit()
+            else:
+                query = 'UPDATE rate SET emoji=%s, rate_time=NOW() WHERE item_id=%s AND email=%s'
+                cursor.execute(query, (rate, item_id, email))
+                conn.commit()
+        cursor.close()
+        if rate == '' and comment=='':
+            return render_template('rate_comment.html', error='Please rate or comment')
+        else:
+            return redirect(url_for('/home'))
+    except:
+        return render_template('rate_comment.html', item_id=item_id)
 
 # @app.route('/select_blogger')
 # def select_blogger():
